22524c6bb91aab374d677e41464b075d9da0b75e
Antoine <antoine.schmidt@web.de>
2018-12-12 17:39:16 +0100

    model loading

--- exercise4/test_carracing.py
+++ exercise4/test_carracing.py
@@ -16,12 +16,16 @@ if __name__ == "__main__":
 
     #TODO: Define networks and load agent
     # ....
+    Q = CNN(hl, num_actions)
+    Q_target = CNNTargetNetwork(hl, num_actions)
+    agent = DQNAgent(Q, Q_target, num_actions, exploration_type='boltzmann')
+    agent.load(os.path.join("./models_carracing", "dqn_agent.ckpt"))
 
     n_test_episodes = 15
 
     episode_rewards = []
     for i in range(n_test_episodes):
-        stats = run_episode(env, agent, deterministic=True, do_training=False, rendering=True)
+        stats = run_episode(env, agent, deterministic=True, do_training=False, rendering=True, history_length=history_length)
         episode_rewards.append(stats.episode_reward)
 
     # save results in a dictionary and write them into a .json file
